1441d47c4a526689c83eba79c4ed4cdf328d2153
Sean McEligot <sean.mceligot@gmail.com>
2021-12-23 20:57:01 -0500

    use arch packaged language servers
    /usr/bin/ccls is owned by ccls 0.20210330-4
    /usr/bin/pyright is owned by pyright 1.1.198-1
    /usr/bin/rust-analyzer is owned by rust-analyzer 20211220-1
    /usr/bin/lua-language-server is owned by lua-language-server 2.4.7-1

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,6 +3,17 @@ if not status_ok then
   return
 end
 
-require "user.lsp.lsp-installer"
-require("user.lsp.handlers").setup()
+local lspconfig = require'lspconfig'
+
+--require "user.lsp.lsp-installer"
+local lsp_handlers = require("user.lsp.handlers")
+lsp_handlers.setup()
 require "user.lsp.null-ls"
+require ("user.lsp.sumneko_lua")
+lspconfig.sumneko_lua.setup{ on_attach = lsp_handlers.on_attach }
+require ("user.lsp.pyright")
+lspconfig.pyright.setup{ on_attach = lsp_handlers.on_attach }
+require "user.lsp.rust_analyzer"
+lspconfig.rust_analyzer.setup{ on_attach = lsp_handlers.on_attach }
+require "user.lsp.ccls"
+lspconfig.ccls.setup{ on_attach = lsp_handlers.on_attach }
